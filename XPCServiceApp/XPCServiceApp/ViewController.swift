8da6c614aa91ecfe6c931623f18356f68598f3fe
이진하 <dkwlsfk2323@gmail.com>
2022-03-15 23:33:30 +0900

    create project

---
+++ XPCServiceApp/XPCServiceApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  XPCServiceApp
+//
+//  Created by 이진하 on 2022/03/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
